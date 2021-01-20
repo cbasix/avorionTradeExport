5ebaccf99b79e17a7a8233c22534e97f3842820c
Cbasix <cbasix@users.noreply.github.com>
2021-01-20 18:06:16 +0100

    update compatible avorion versions

--- modinfo.lua
+++ modinfo.lua
@@ -13,31 +13,20 @@ meta =
     -- Title of your mod that will be displayed to players
     title = "TradeExport",
 
+    -- Type of your mod, either "mod" or "factionpack"
+    type = "mod",
+
     -- Description of your mod that will be displayed to players
-    description = [[
-Exports the trading goods data as .csv files, usable for data mining purposes ;).
-
-It only exports the data you can see with the basic trading system. Export is triggered when one of your ships equiped with a trading system enters a sector. Data is only updated if a ship of yours visits a sector again.
-
-It exports one .csv file per sector. The files are saved in `.avorion/moddata/` and are named `TradeExport-{galaxy_seed}-sector{x_coord}-{y_coord}.csv`. Rows have the following format:
-
-```
-action ('buy' or 'sell' to station); good; price; stock; max_stock; coord_x; coord_y; station; size (of good)
-```
-
-Sectors without any trading goods generate empty files.
-
-The datamining folder contains an python example for working with the exported data.
-
-Note: Mod is currently only tested running under linux. If you experience any problems don't hesitate to open an issue at https://github.com/cbasix/avorionTradeExport
-    ]],
+    description = "Exports the trading goods data as .csv files, usable for data mining purposes ;).\n\nIt only exports the data you can see with the basic trading system. Export is triggered when one of your ships equiped with a trading system enters a sector. Data is only updated if a ship of yours visits a sector again.\n\nIt exports one .csv file per sector. The files are saved in `.avorion/moddata/` and are named `TradeExport-{galaxy_seed}-sector{x_coord}-{y_coord}.csv`. Rows have the following format:\n\n```\naction ('buy' or 'sell' to station); good; price; stock; max_stock; coord_x; coord_y; station; size (of good)\n```\n\nSectors without any trading goods generate empty files.\n\nThe datamining folder contains an python example for working with the exported data.\n\nNote: Mod is currently only tested running under linux. If you experience any problems don't hesitate to open an issue at https://github.com/cbasix/avorionTradeExport\n    ",
 
     -- Insert all authors into this list
     authors = {"masterix"},
 
     -- Version of your mod, should be in format 1.0.0 (major.minor.patch) or 1.0 (major.minor)
-    -- This will be used to check for unmet dependencies or incompatibilities
-    version = "1.2",
+    -- This will be used to check for unmet dependencies or incompatibilities, and to check compatibility between clients and dedicated servers with mods.
+    -- If a client with an unmatching major or minor mod version wants to log into a server, login is prohibited.
+    -- Unmatching patch version still allows logging into a server. This works in both ways (server or client higher or lower version).
+    version = "1.2.1",
 
     -- If your mod requires dependencies, enter them here. The game will check that all dependencies given here are met.
     -- Possible attributes:
@@ -55,7 +44,7 @@ Note: Mod is currently only tested running under linux. If you experience any pr
     --      {id = "OptionalMod", min = "0.2", optional = true}, -- we support OptionalMod optionally, starting at version 0.2
     -- },
     dependencies = {
-        {id = "Avorion", min = "0.29", max = "0.40"}
+        {id = "Avorion", min = "0.29", max = "1.40"}
     },
 
     -- Set to true if the mod only has to run on the server. Clients will get notified that the mod is running on the server, but they won't download it to themselves
